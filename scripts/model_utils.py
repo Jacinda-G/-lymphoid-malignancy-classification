82ab107a8165b567648bd3b42d3742f6f85fbd0f
Frankie Pincheputo <garcia.jacinda@yahoo.com>
2025-07-19 13:21:35 -0700

    updatated model url to sas token

--- scripts/model_utils.py
+++ scripts/model_utils.py
@@ -10,7 +10,7 @@ def load_model(device="cpu"):
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
 
     if not os.path.exists(model_path):
-        url = "https://lymphomamlws4085132117.blob.core.windows.net/models/trained_resnet50.pth"
+        url = "https://lymphomamlws4085132117.blob.core.windows.net/models/trained_resnet50.pth?sp=r&st=2025-07-19T20:05:11Z&se=2025-08-03T04:20:11Z&spr=https&sv=2024-11-04&sr=b&sig=8rV6qeHMV7ni81Yt0vxWsyroxF1Z3IoKh06Ui3RrDFw%3D"
         urllib.request.urlretrieve(url, model_path)
 
     model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
